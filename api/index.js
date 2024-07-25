1720f3e6b969c97e58930d47cf6facccebb0617a
JUIN TAN <tanjuinlong@gmail.com>
2024-07-25 15:58:49 +0800

    add middleware for cors

--- api/index.js
+++ api/index.js
@@ -15,6 +15,12 @@ const multer = require("multer");
 const fs = require("fs");
 const path = require('path');
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(cors({
     credentials: true,
     // origin: "https://airbnb-clone-mern-api.vercel.app"
